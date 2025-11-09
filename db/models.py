321544ccdb6bba731f25df0492bdb8f631ab3205
tresly <litvinalex632@gmail.com>
2025-11-09 21:53:13 +0100

    Solution

--- db/models.py
+++ db/models.py
@@ -109,7 +109,8 @@ class Ticket(models.Model):
         ]
 
     def __str__(self) -> str:
-        return (f"{self.movie_session.movie.title}"
-                f" {self.movie_session.show_time.
-                strftime("%Y-%m-%d %H:%M:%S")}"
-                f" (row: {self.row}, seat: {self.seat})")
+        return (
+            f"{self.movie_session.movie.title}"
+            f" {self.movie_session.show_time.strftime("%Y-%m-%d %H:%M:%S")}"
+            f" (row: {self.row}, seat: {self.seat})"
+        )
